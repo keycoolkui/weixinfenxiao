b662c11a92613e846700d91fad3c5c742c1d3744
keycoolkui <37068701@qq.com>
2016-01-01 12:51:03 +0800

    edit share

--- addons/eso_sale/site.php
+++ addons/eso_sale/site.php
@@ -1141,7 +1141,8 @@ class Eso_SaleModuleSite extends WeModuleSite {
 		$share = "eso_saleshareQrcode".$_W['uniacid'];
 		if($_COOKIE[$share] != $_W['uniacid']."share".$id){
 			include "mobile/phpqrcode.php";//引入PHP QR库文件
-			$value = $_W['siteroot']."app/".$this->mturl('list',array('mid'=>$id));
+			// $value = $_W['siteroot']."app/".$this->mturl('list',array('mid'=>$id));
+			$value = $_W['siteroot']."app/".$this->mturl('register',array('agent'=>1));
 			$errorCorrectionLevel = "L";
 			$matrixPointSize = "4";
 			$imgname = "share$id.png";
