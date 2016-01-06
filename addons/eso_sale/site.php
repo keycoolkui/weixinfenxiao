06232ad824d840d14067e012a02faa386e5becd7
keycoolkui <37068701@qq.com>
2016-01-06 11:37:52 +0800

    test

--- addons/eso_sale/site.php
+++ addons/eso_sale/site.php
@@ -4274,6 +4274,20 @@ class Eso_SaleModuleSite extends WeModuleSite {
 	private  function getFromUser() {
 		global $_W,$_GPC;
 		$appid = $_W['account']['key'];
+
+		if(empty($_SESSION['uid'])){
+			$state = 'we7sid-'.$_W['session_id'];
+			$_SESSION['dest_url'] = base64_encode($_SERVER['QUERY_STRING']);
+
+			$url = $_W['siteroot'] . "app/index.php?i={$_W['uniacid']}&j={$_W['acid']}&c=auth&a=oauth&scope=userinfo";
+			$callback = urlencode($url);
+
+			$forward = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid='.$_W['oauth_account']['key'].'&redirect_uri='.$callback.'&response_type=code&scope=snsapi_userinfo&state='.$state.'#wechat_redirect';
+			header('Location: '.$forward);
+			exit;
+		}
+		return $_SESSION['uid'];
+
 		if(false)
 		{
 			return $_W['fans']['from_user'];
@@ -4293,15 +4307,7 @@ class Eso_SaleModuleSite extends WeModuleSite {
 				return $_W['fans']['from_user'];
 			}
 		}else{
-			$state = 'we7sid-'.$_W['session_id'];
-			$_SESSION['dest_url'] = base64_encode($_SERVER['QUERY_STRING']);
 
-			$url = $_W['siteroot'] . "app/index.php?i={$_W['uniacid']}&j={$_W['acid']}&c=auth&a=oauth&scope=userinfo";
-			$callback = urlencode($url);
-
-			$forward = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid='.$_W['oauth_account']['key'].'&redirect_uri='.$callback.'&response_type=code&scope=snsapi_userinfo&state='.$state.'#wechat_redirect';
-			header('Location: '.$forward);
-			exit;
 			//checkauth();
 //			var_dump($_W);exit;
 //			mc_oauth_userinfo();
